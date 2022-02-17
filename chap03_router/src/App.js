786cf086df19336a3f67fb17587dcf18bc17625b
CODE0000 <thfxm0601@gmail.com>
2022-02-17 16:38:27 +0900

    0217_4
    라우터

--- chap03_router/src/App.js
+++ chap03_router/src/App.js
@@ -1,19 +1,53 @@
-
 import 'bootstrap/dist/css/bootstrap.css';
+// npm i bootstrap@4
+// npm un react-router  => 6버전 삭제
+// npm i react-router@5 
 
-// npm i react-router react-router-dom
+// npm un react-router-dom => 6버전 삭제
+// npm i react-router-dom@5
 // npm i react-loader-spinner
 
+import { Link, Route } from 'react-router-dom';
+import A01FunctionProps from './components/A01FunctionProps';
+import A02FunctionState from './components/A02FunctionState';
+import A03Currency from './components/A03Currency';
+import A04History from './components/A04History';
+
 function App() {
-  const user = {name: 'HungBu', age: 20};
+  const user = { name: 'HungBu', age: 20 };
   const ary = ['a', 'b', 'c'];
   const onPlus = (x, y) => {
     return `${x} + ${y} = ${x + y}`;
-  }
+  };
 
   return (
     <div className="card-body">
-      
+      <h1>Router</h1>
+
+      <div>
+        <Link to="/">Index</Link>  | {' '}
+        <Link to="/props">props</Link>  |{' '}
+        <Link to="/state">State</Link>  |{' '}
+        <Link to="/currency">Currency</Link>  |{' '}
+        <Link to="/history">History</Link>  |{' '}
+
+      </div>
+      <hr></hr>
+
+      {/* Route가 기술된 영역에 지정한 컴퍼넌트가 표시된다 */}
+      {/* exact={true} -> 정확히 일치하는 패스만 나옴 */}
+      <Route path="/" exact={true} render={() => <div>
+        <h1>Index Page</h1>
+        <div>Hello World</div>
+      </div>} />
+      {/* props 값을 전달할 필요가 있는 경우 */}
+      {/*A01FunctionProps에 props한거 다 있어야함  */}
+      <Route path="/props" render={() => <A01FunctionProps
+        name="NolBu" age={20} arr={ary} user={user} onPlus={onPlus} isChecked={true} />} />
+      {/* props 값 전달 없이 순수하게 표시만 하는 경우 */}
+      <Route path="/state" component={A02FunctionState}></Route>
+      <Route path="/currency" component={A03Currency}></Route>
+      <Route path="/history" component={A04History}></Route>
     </div>
   );
 }
--- chap03_router/src/components/A04History.js
+++ chap03_router/src/components/A04History.js
@@ -1,7 +1,15 @@
 
 import React from 'react';
 
-const A02ProductComponent = () => {
+const A02ProductComponent = (props) => {
+
+    const { history } = props;
+    function back() {
+        history.goBack();
+    }
+    const forword = () => history.goForward();
+    const home = () => history.push('/');
+    const assign = (url) => history.push(url);
 
     return (
         <div>
@@ -10,12 +18,12 @@ const A02ProductComponent = () => {
             <br />
 
             <div>
-                <button>BACK</button>
-                <button>FORWARD</button>
-                <button>HOME</button>
-                <button>PARAMETER</button>
+                <button onClick={back}>BACK</button>
+                <button onClick={forword}>FORWARD</button>
+                <button onClick={home}>HOME</button>
+                <button onClick={() => assign('/currency')}>PARAMETER</button>
             </div>
         </div>
-    )
-}
+    );
+};
 export default A02ProductComponent;
